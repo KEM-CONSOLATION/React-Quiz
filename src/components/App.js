edef23d8900c13c23075e9aac5a43bb64a78fa6d
Kem-consolation <consolationlotachi@gmail.com>
2023-07-21 16:21:45 +0100

    Options added

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import Error from "./Error";
 import Main from "./Main";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
+import NextButton from "./NextButton";
+import Progress from "./Progress";
 
 export default function App() {
   const initialState = {
@@ -14,13 +16,13 @@ export default function App() {
     status: "loading",
     index: 0,
     answer: null,
-    point: 0,
+    points: 0,
   };
   function reducer(state, action) {
     switch (action.type) {
       case "dataReceived":
         return { ...state, questions: action.payload, status: "ready" };
-      case "datafailed":
+      case "dataFailed":
         return { ...state, status: "error" };
       case "start":
         return { ...state, status: "active" };
@@ -34,17 +36,23 @@ export default function App() {
               ? state.points + question.points
               : state.points,
         };
+      case "nextQuestion":
+        return { ...state, index: state.index + 1, answer: null };
       default:
         throw new Error("Action unknown");
     }
   }
 
-  const [{ questions, status, index, answer }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
     reducer,
     initialState
   );
 
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
   useEffect(() => {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
@@ -62,11 +70,20 @@ export default function App() {
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
         {status === "active" && (
-          <Question
-            question={questions[index]}
-            dispatch={dispatch}
-            answer={answer}
-          />
+          <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton dispatch={dispatch} answer={answer} />
+          </>
         )}
       </Main>
     </div>
